0845b1747840dcb348ce904e90d1f9bdef7483c0
toonMarty <martinnyaga7373@gmail.com>
2023-02-02 13:28:22 -0800

    Basic Annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""This module contains a function that adds two
+float values
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    This function adds two float values, a and b
+    that have been annotated
+    Args:
+        a (float): first value
+        b (float): second value
+    Return:
+        a + b (float): the sum of the float values
+    """
+    return a + b
