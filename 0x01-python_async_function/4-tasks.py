e9e945faae9c964911ae9ddbc2937269b27d8745
toonMarty <martinnyaga7373@gmail.com>
2023-02-06 11:36:44 -0800

    Tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This module contains a coroutine wait_n that
+returns the list of all the delays
+"""
+
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    This function wait_n returns the list of all the delays.
+    The list of the delays are in ascending order
+    without using sort() because of concurrency
+    Args:
+        n (int): the number of times to spawn wait_random
+        max_delay (int): the maximum delay specified
+    Return:
+        sorted_elapsed_time(list): a sorted list of delays
+    """
+    elapsed_time = []
+    sorted_elapsed_time = []
+
+    for i in range(n):
+        task = await task_wait_random(max_delay)
+        elapsed_time.append(task)
+
+    while elapsed_time:
+        smallest = elapsed_time[0]
+        for x in elapsed_time:
+            if x < smallest:
+                smallest = x
+        sorted_elapsed_time.append(smallest)
+        elapsed_time.remove(smallest)
+    return sorted_elapsed_time
