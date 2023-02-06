2ce92158653b0fa61734c913bc25153d9d103eea
toonMarty <martinnyaga7373@gmail.com>
2023-02-06 12:11:47 -0800

    sorted

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -26,11 +26,4 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
         task = await asyncio.create_task(wait_random(max_delay))
         elapsed_time.append(task)
 
-    while elapsed_time:
-        smallest = elapsed_time[0]
-        for x in elapsed_time:
-            if x < smallest:
-                smallest = x
-        sorted_elapsed_time.append(smallest)
-        elapsed_time.remove(smallest)
-    return sorted_elapsed_time
+    return sorted(elapsed_time)
